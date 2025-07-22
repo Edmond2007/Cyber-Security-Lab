37d196b72452c0658c1b8f8f27f0d02a6e30a9d4
Edmond2007 <edmondseropyan6@gmail.com>
2025-07-22 19:29:30 +0000

    create: wlan0 monitor mode script

---
+++ start-mon.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+clear
+
+echo "What do you want to do?"
+
+select action in "Start Monitor Mode" "Stop Monitor Mode" "Exit"; do
+    case $action in
+        "Start Monitor Mode")
+            airmon-ng check kill
+	    airmon-ng start wlan0
+            echo "wlan0 in monitor mode"
+            break
+            ;;
+        "Stop Monitor Mode")
+	    airmon-ng stop wlan0
+	    systemctl start wpa_supplicant
+	    systemctl start NetworkManager
+            echo "Monitor Mode Stoped"
+            break
+            ;;
+        "Exit")
+            echo "Exiting..."
+            exit 0
+            ;;
+        *)
+            echo "Invalid option."
+            ;;
+    esac
+done
